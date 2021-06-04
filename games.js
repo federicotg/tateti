c46178241594c195898cffb353fe50ca21c78e2a
Federico Tello Gentile <federicotg@gmail.com>
2021-06-04 19:42:22 -0300

    fix

--- games.js
+++ games.js
@@ -5,7 +5,7 @@ const express = require('express');
 const tateti = require('./tateti.js');
 
 const app = express();
-const PORT = 3000;
+const PORT = 80;
 
 app.use(express.json());
 app.use(express.static('public'));
